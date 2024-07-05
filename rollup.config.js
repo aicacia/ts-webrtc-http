726814749cdee173c17b438c7596736cbbd2ccfa
Nathan Faucett <nathanfaucett@gmail.com>
2024-07-05 06:06:08 -0400

    format

--- rollup.config.js
+++ rollup.config.js
@@ -5,31 +5,31 @@ import terser from "@rollup/plugin-terser";
 import esmImportToUrl from "rollup-plugin-esm-import-to-url";
 
 export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: "browser/index.js",
-        format: "es",
-        sourcemap: true,
-        plugins: [terser()],
-      },
-    ],
-    plugins: [
-      esmImportToUrl({
-        imports: {
-          "@aicacia/hash": "https://unpkg.com/@aicacia/hash@0/browser/index.js",
-          "@aicacia/rand": "https://unpkg.com/@aicacia/rand@0/browser/index.js",
-          tslib: "https://unpkg.com/tslib@2/tslib.es6.js",
-        },
-      }),
-      resolve({ browser: true }),
-      commonjs({
-        transformMixedEsModules: true,
-      }),
-      typescript({
-        tsconfig: "./tsconfig.esm.json",
-      }),
-    ],
-  },
+	{
+		input: "src/index.ts",
+		output: [
+			{
+				file: "browser/index.js",
+				format: "es",
+				sourcemap: true,
+				plugins: [terser()],
+			},
+		],
+		plugins: [
+			esmImportToUrl({
+				imports: {
+					"@aicacia/hash": "https://unpkg.com/@aicacia/hash@0/browser/index.js",
+					"@aicacia/rand": "https://unpkg.com/@aicacia/rand@0/browser/index.js",
+					tslib: "https://unpkg.com/tslib@2/tslib.es6.js",
+				},
+			}),
+			resolve({ browser: true }),
+			commonjs({
+				transformMixedEsModules: true,
+			}),
+			typescript({
+				tsconfig: "./tsconfig.esm.json",
+			}),
+		],
+	},
 ];
